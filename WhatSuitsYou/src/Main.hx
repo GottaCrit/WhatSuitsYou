f1b51d41a00e16626d26d2616602c2ef27aaf8f6
Mike Oosterloo <faillling25@gmail.com>
2016-01-08 10:00:28 +0100

    First Commit
    Fixing some initial problems

--- WhatSuitsYou/src/Main.hx
+++ WhatSuitsYou/src/Main.hx
@@ -1,21 +1,52 @@
 package;
 
+import openfl.Assets;
+
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
 import openfl.display.Sprite;
-import openfl.Lib;
+
+import openfl.events.KeyboardEvent;
+
+import openfl.Lib; 
 
 /**
- * ...
- * @author Mike
+ * 
+ * @author Evan, Max, Mike, Robin, Sipan
+ * 
  */
+
+enum Gamestate{
+	MainMenu;
+	PickingState;
+	PlatformState;
+	Highscores;
+}
+ 
 class Main extends Sprite 
 {
-
+	var curretGamestate:Gamestate;
+	
 	public function new() 
 	{
 		super();
-		
-		// Assets:
-		// openfl.Assets.getBitmapData("img/assetname.jpg");
-	}
 
+	}
+	
+	function keyPressed( event:KeyboardEvent )
+	{
+		if( /*event.keyCode == Keyboard.UP || */ event.keyCode == 87)
+		{
+			
+		}else if( /*event.keyCode == Keyboard.LEFT || */ event.keyCode == 65)
+		{
+			
+		}else if( /*event.keyCode == Keyboard.RIGHT || */ event.keyCode == 83)
+		{
+			
+		}else if( /*event.keyCode == Keyboard.DOWN || */ event.keyCode == 68)
+		{
+			
+		}
+	}
 }
